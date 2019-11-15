cb805cd8fbe664449b2e669f1ac1a1eb5751a7ed
MostafaSalman <56025689+MostafaSalman@users.noreply.github.com>
2019-11-15 21:25:51 +0200

    Update main.c

--- main.c
+++ main.c
@@ -3,22 +3,23 @@
 #include"app.h"
 #include"menu.h"
 
+#define NUM_ARR 3
+#define NUM_ARR2 2
+#define NUM_ARR3 1
+
 int main(void)
 {
 	char input;
-	int num =3;
-	int num2 = 2;
-	int num3 = 1;
-	Menu *array = malloc(sizeof(Menu) * num);
-	Menu *array2 = malloc(sizeof(Menu) * num2);
-	Menu *array3 = malloc(sizeof(Menu) * num3);
+	Menu *array = malloc(sizeof(Menu) * NUM_ARR);
+	Menu *array2 = malloc(sizeof(Menu) * NUM_ARR2);
+	Menu *array3 = malloc(sizeof(Menu) * NUM_ARR3);
 	
 	array[0].ch = 'a';
 	array[0].desc = "Excute func1";
 	array[0].type = false;
 	array[0].task_ptr = &func1;
 	array[0].parrent_arr = array;
-	array[0].parrent_arr_len = num;
+	array[0].parrent_arr_len = NUM_ARR;
 	array[1].ch = 'b';
 	array[1].desc = "Excute func2";
 	array[1].type = false;
@@ -27,26 +28,26 @@ int main(void)
 	array[2].desc = "Other functions >";
 	array[2].type = true;
 	array[2].Next.child_arr = array2;
-	array[2].Next.child_arr_len = num2;
+	array[2].Next.child_arr_len = NUM_ARR2;
 	
 	array2[0].ch = 'd';
 	array2[0].desc = "Excute func3";
 	array2[0].type = false;
 	array2[0].task_ptr = &func3;
 	array2[0].parrent_arr = array;
-	array2[0].parrent_arr_len = num;
+	array2[0].parrent_arr_len = NUM_ARR;
 	array2[1].ch = 'e';
 	array2[1].desc = "Other >";
 	array2[1].type = true;
 	array2[1].Next.child_arr = array3;
-	array2[1].Next.child_arr_len = num3;
+	array2[1].Next.child_arr_len = NUM_ARR3;
 	
 	array3[0].ch = 'f';
 	array3[0].desc = "Excute func4";
 	array3[0].type = false;
 	array3[0].task_ptr = &func4;
 	array3[0].parrent_arr = array2;
-	array3[0].parrent_arr_len = num2;
+	array3[0].parrent_arr_len = NUM_ARR2;
 	
 	do{
 		printf("\n");
@@ -56,7 +57,7 @@ int main(void)
 		scanf("%c",&input);
 	}while((input != '?')&&(input != 'X'));
 	if(input == '?')
-		Menu_func(array, num);
+		Menu_func(array, NUM_ARR);
 	else if(input == 'X')
 		return 0;
 
