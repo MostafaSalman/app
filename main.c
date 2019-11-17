0a02eb4cd7c0b72fc160a3aee02b6e18cf2c2511
MostafaSalman <56025689+MostafaSalman@users.noreply.github.com>
2019-11-17 15:54:32 +0200

    Update main.c

--- main.c
+++ main.c
@@ -3,60 +3,51 @@
 #include"app.h"
 #include"menu.h"
 
-#define NUM_ARR 3
-#define NUM_ARR2 2
-#define NUM_ARR3 1
+#define NUM_ARR 3						/* Number Of Items Of Main Menu */
+#define NUM_ARR2 2						/* Number Of Items Of First Sub Menu */
+#define NUM_ARR3 1						/* Number Of Items Of Second Sub Menu */
 
-int main(void)
-{
+char exit_ch ;
+char back_ch ;
+
+int main(void){
 	char input;
+	char display = '?';					/* Character To Display Menu When User Choose It */
+	exit_ch = 'X';						/* Character To Exit When User Choose It */
+	back_ch = 'B';						/* Character To Back To The Previous Menu When User Choose It */
+	
+	//Allocate Array For Each Menu
 	Menu *array = malloc(sizeof(Menu) * NUM_ARR);
 	Menu *array2 = malloc(sizeof(Menu) * NUM_ARR2);
 	Menu *array3 = malloc(sizeof(Menu) * NUM_ARR3);
 	
-	array[0].ch = 'a';
-	array[0].desc = "Excute func1";
-	array[0].type = false;
-	array[0].task_ptr = &func1;
-	array[0].parrent_arr = array;
-	array[0].parrent_arr_len = NUM_ARR;
-	array[1].ch = 'b';
-	array[1].desc = "Excute func2";
-	array[1].type = false;
-	array[1].task_ptr = &func2;
-	array[2].ch = 'c';
-	array[2].desc = "Other functions >";
-	array[2].type = true;
-	array[2].Next.child_arr = array2;
-	array[2].Next.child_arr_len = NUM_ARR2;
+	//Creat Main Menu
+	Menu Item_1 = {.ch='a', .desc="Excute func1", .type=false, .task_ptr = &func1};
+	Menu Item_2 = {.ch='b', .desc="Excute func2", .type=false, .task_ptr = &func2};
+	Menu Item_3 = {.ch='c', .desc="Other functions >", .type=true , .Next.child_arr = array2, .Next.child_arr_len = NUM_ARR2};
+	Menu *main_menu[NUM_ARR] = {&Item_1, &Item_2, &Item_3};
+	Creat_Menu(array, NUM_ARR, main_menu);
 	
-	array2[0].ch = 'd';
-	array2[0].desc = "Excute func3";
-	array2[0].type = false;
-	array2[0].task_ptr = &func3;
-	array2[0].parrent_arr = array;
-	array2[0].parrent_arr_len = NUM_ARR;
-	array2[1].ch = 'e';
-	array2[1].desc = "Other >";
-	array2[1].type = true;
-	array2[1].Next.child_arr = array3;
-	array2[1].Next.child_arr_len = NUM_ARR3;
+	//Creat First Sub Menu
+	Menu Item_4 = {.ch='d', .desc="Excute func3", .type=false, .task_ptr = &func3};
+	Menu Item_5 = {.ch='e', .desc="Other >", .type=true , .Next.child_arr = array3, .Next.child_arr_len = NUM_ARR3};
+	Menu *first_submenu[NUM_ARR2] = {&Item_4, &Item_5};
+	Creat_Menu(array2, NUM_ARR2, first_submenu);
 	
-	array3[0].ch = 'f';
-	array3[0].desc = "Excute func4";
-	array3[0].type = false;
-	array3[0].task_ptr = &func4;
-	array3[0].parrent_arr = array2;
-	array3[0].parrent_arr_len = NUM_ARR2;
+	//Creat Second Sub Menu
+	Menu Item_6 = {.ch='f', .desc="Excute func4", .type=false, .task_ptr = &func4};
+	Menu *second_submenu[NUM_ARR2] = {&Item_6};
+	Creat_Menu(array3, NUM_ARR3, second_submenu);
 	
+	//Display Welcome Message And Wait The Users Choise To Display The Main Menu 
 	do{
 		printf("\n");
 		system("clear");
 		printf("\n**********welcome to our system**********\n"
-			"\nTo display our menu press ?\nTo exit press X \napp>");
+			"\nTo display our menu press %c\nTo exit press X \napp>",display);
 		scanf("%c",&input);
-	}while((input != '?')&&(input != 'X'));
-	if(input == '?')
+	}while((input != display)&&(input != 'X'));
+	if(input == display)
 		Menu_func(array, NUM_ARR);
 	else if(input == 'X')
 		return 0;
